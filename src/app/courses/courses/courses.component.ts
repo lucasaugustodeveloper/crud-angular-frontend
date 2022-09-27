ade9077c491418e5164d7c5971e78341ffa01994
Lucas Augusto <lucas.augusto5061@gmail.com>
2022-09-27 12:55:38 -0300

    add: add service for get information of API

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,19 +1,23 @@
-import { Course } from '../model/course';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
+
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'front-end'}
-  ];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor() { }
+  constructor(private coursesService: CoursesService) {
+    this.courses = this.coursesService.list();
+  }
 
   ngOnInit(): void {
   }
